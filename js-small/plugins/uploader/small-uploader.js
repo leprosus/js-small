802c1da2417f86a2e7aef69fcb52dcbcf0913292
leprosus <admin@leprosus.ru>
2012-08-01 15:11:31 +0300

    Refactored code & style Uploader Plug-in

--- js-small/plugins/uploader/small-uploader.js
+++ js-small/plugins/uploader/small-uploader.js
@@ -1,5 +1,5 @@
 /*
- * JS-Small JavaScript Framework Plugin 0.1.1
+ * JS-Small JavaScript Framework Plugin
  * Description: Plug-in allows to upload files
  * Copyright (c) 2008 - 2012 Denis Korolev
  * Released under the MIT License.
@@ -7,12 +7,14 @@
  *                   http://www.js-small.com/
  * Project support:  http://www.evalab.ru/
  *                   http://www.evalab.com/
+ * @author Denis Korolev
+ * @version 0.1.1
  */
 small.extendFunctions({
     uploader: function(options){
-        if(small.typeIn(options, "object")){
-            var name = options.name || "file",
-            url = options.url || "",
+        if(small.typeIn(options, 'object')){
+            var name = options.name || 'file',
+            url = options.url || '',
             extensions = options.extensions || [],
             maxSize = options.maxSize || 0,
             onError = options.onError || function(){},
@@ -20,38 +22,38 @@ small.extendFunctions({
             onComplete = options.onComplete || function(){},
             onCancel = options.onCancel || function(){};
 
-            var fileName = "",
-            form = small.create("form").attr({
-                "name": name,
-                "action": small.base().concat(url),
-                "method": "post",
-                "enctype": "multipart/form-data",
-                "target": name
+            var fileName = '',
+            form = small.create('form').attr({
+                'name': name,
+                'action': small.base().concat(url),
+                'method': 'post',
+                'enctype': 'multipart/form-data',
+                'target': name
             }),
-            uploader = form.append("div.uploader").hide(),
-            iframe = small.create("iframe").attr({
-                "name": name,
-                "src": "javascript:;"
+            uploader = form.append('div.uploader').hide(),
+            iframe = small.create('iframe').attr({
+                'name': name,
+                'src': 'javascript:;'
             }).hide();
             form.append(iframe);
 
             if(maxSize > 0){
-                var hidden = small.create("input").attr({
-                    "type": "hidden",
-                    "name": "MAX_FILE_SIZE",
-                    "value": maxSize
+                var hidden = small.create('input').attr({
+                    'type': 'hidden',
+                    'name': 'MAX_FILE_SIZE',
+                    'value': maxSize
                 });
                 form.append(hidden);
             }
 
-            var file = small.create("input").attr({
-                "type": "file",
-                "name": name
+            var file = small.create('input').attr({
+                'type': 'file',
+                'name': name
             }).change(function(){
                 var current = small(this), error = false;
-                fileName = current.attr("value").replace(/^.*(\\|\/|\:)/, '');
+                fileName = current.attr('value').replace(/^.*(\\|\/|\:)/, '');
                 if(extensions.length > 0){
-                    var regExp = new RegExp("\\.(" + small.trim(extensions).join("|") + ")$", "i");
+                    var regExp = new RegExp('\\.(' + small.trim(extensions).join('|') + ')$', 'i');
                     error = !regExp.test(fileName);
                 }
 
@@ -62,29 +64,29 @@ small.extendFunctions({
 
                     var progress = 0;
                     file.hide();
-                    uploader.show().css("width", width + "px")
-                    .append("div.cancel").click(function(){
+                    uploader.show().css('width', width + 'px')
+                    .append('div.cancel').click(function(){
                         var newUploader = small.uploader(options);
                         uploader.after(newUploader);
                         uploader.remove();
                         iframe.remove();
                         onCancel.call(form.node(), name, options);
                     });
-                    var value = uploader.append("div.border").append("div.value");
-                    value.append("div.animation").opacity(70);
+                    var value = uploader.append('div.border').append('div.value');
+                    value.append('div.animation').opacity(70);
                     value.start({
-                        "time": 100,
-                        "callback": function(){
-                            value.css("width", ((width - 16) * progress++ / 100) + "px")
+                        'time': 100,
+                        'callback': function(){
+                            value.css('width', ((width - 16) * progress++ / 100) + 'px')
                         },
-                        "repeat": 90
+                        'repeat': 90
                     });
 
                     iframe.load(function(){
                         var idoc = iframe.node().contentDocument || iframe.node().contentWindow.document,
                         content = idoc.body.innerHTML;
                         iframe.remove();
-                        uploader.empty().append("div.done").text(fileName);
+                        uploader.empty().append('div.done').text(fileName);
                         onComplete.call(form.node(), name, options, content);
                     });
                 } else onError.call(form.node(), name, options);
@@ -96,16 +98,16 @@ small.extendFunctions({
     }
 });
 small.extendMethods({
-    "appendUploader": function(options){
+    'appendUploader': function(options){
         return small(this).append(small.uploader(options));
     },
-    "prependUploader": function(options){
+    'prependUploader': function(options){
         return small(this).prepend(small.uploader(options));
     },
-    "afterUploader": function(options){
+    'afterUploader': function(options){
         return small(this).after(small.uploader(options));
     },
-    "beforeUploader": function(options){
+    'beforeUploader': function(options){
         return small(this).before(small.uploader(options));
     }
 });
